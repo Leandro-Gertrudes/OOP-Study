bf3906ef11303d8ea7831c0ca6542d304ee014a0
Leandro Gertrudes Pinheiro <lgertrud@c2r1s4.42porto.com>
2026-04-14 17:08:25 +0100

    add ex00

---
+++ module06/ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgertrud <lgertrud@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/14 17:01:34 by lgertrud          #+#    #+#             */
+/*   Updated: 2026/04/14 17:01:57 by lgertrud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: ./convert <literal>\n";
+        return 1;
+    }
+
+    ScalarConverter::convert(argv[1]);
+}
