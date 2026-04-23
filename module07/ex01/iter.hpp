3a2520cea45f9e5a1be57b3eab512bfc128559c5
Leandro Gertrudes <leandro200055@hotmail.com>
2026-04-23 16:21:27 +0100

    fixed ex02

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: lgertrud <lgertrud@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 14:26:17 by lgertrud          #+#    #+#             */
-/*   Updated: 2026/04/23 14:53:48 by lgertrud         ###   ########.fr       */
+/*   Updated: 2026/04/23 16:20:47 by lgertrud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@
 #include <string>
 
 
-template <typename T, typename L, typename F>
-void iter(T *array, L const lenght, F f){
-	for(L i = 0; i < lenght; i++)
-		f(array[i]);
+template <typename T, typename F>
+void iter(T *array, size_t const length, F f) {
+    for (size_t i = 0; i < length; i++)
+        f(array[i]);
 }
 
-template <typename T, typename L, typename F>
-void iter(T const *array, L const lenght, F f){
-	for(L i = 0; i < lenght; i++)
-		f(array[i]);
+template <typename T, typename F>
+void iter(T const *array, size_t const length, F f) {
+    for (size_t i = 0; i < length; i++)
+        f(array[i]);
 }
 
 #endif
